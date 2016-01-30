25e58dd9c34855ba0b0d42c3ada25bbc8d660e76
unknown <easonsu1129@gmail.com>
2016-01-30 11:56:21 -0500

    Set drive base encoders to parameters

--- 2016Project/src/org/usfirst/frc/team131/robot/DriveBase.java
+++ 2016Project/src/org/usfirst/frc/team131/robot/DriveBase.java
@@ -5,6 +5,9 @@ import edu.wpi.first.wpilibj.Talon;
 
 
 public class DriveBase {
+	public static final double ENCODER_PULSES_PER_REVOLUTION = 250.0;
+	public static final double ENCODER_GEAR_RATIO = 1.0;
+	
 	Talon frontLeft = new Talon(PortConstants.LEFT_FRONT_TALON);
 	Talon middleLeft = new Talon (PortConstants.LEFT_MIDDLE_TALON);
 	Talon backLeft = new Talon (PortConstants.LEFT_BACK_TALON);
@@ -38,16 +41,15 @@ public class DriveBase {
 	}
 	public DriveBase() {
 		// TODO Auto-generated constructor stub
-		
+		double distancePerPulse = ENCODER_GEAR_RATIO / ENCODER_PULSES_PER_REVOLUTION;
+				
 		leftEncoder.setMaxPeriod(0.1);
-		leftEncoder.setMinRate(10);
-		leftEncoder.setDistancePerPulse(5);
+		leftEncoder.setDistancePerPulse(distancePerPulse);
 		leftEncoder.setReverseDirection(true);
 		leftEncoder.setSamplesToAverage(7);
 		
 		rightEncoder.setMaxPeriod(0.1);
-		rightEncoder.setMinRate(10);
-		rightEncoder.setDistancePerPulse(5);
+		rightEncoder.setDistancePerPulse(distancePerPulse);
 		rightEncoder.setReverseDirection(true);
 		rightEncoder.setSamplesToAverage(7);
 		
