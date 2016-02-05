51460a5f771ce3cbef6faa2664f8b88748a48bfe
unknown <easonsu1129@gmail.com>
2016-02-04 19:50:44 -0500

    Added arm manual mode to increment setpoint, Cleaned up button assignment, removed ball reversal button

--- 2016Project/src/org/usfirst/frc/team131/robot/Robot.java
+++ 2016Project/src/org/usfirst/frc/team131/robot/Robot.java
@@ -13,138 +13,140 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * directory.
  */
 public class Robot extends IterativeRobot {
-    final String defaultAuto = "Default";
-    final String customAuto = "My Auto";
-    String autoSelected;
-    SendableChooser chooser;
-    OI oi;
-    DriveBase drive;
-    ScalingHook hook;
+	final String defaultAuto = "Default";
+	final String customAuto = "My Auto";
+	String autoSelected;
+	SendableChooser chooser;
+	OI oi;
+	DriveBase drive;
+	ScalingHook hook;
 	IntakeShooter intakeShooter;
 	BallCenterMechanism center;
 	ChaosPot chaosPot;
 	ShoulderArm arm;
 
-    /**
-     * This function is run when the robot is first started up and should be
-     * used for any initialization code.
-     */
-    public void robotInit() {
-        oi = new OI();
-        drive = new DriveBase ();
-        
-    	arm = new ShoulderArm();
-    	center = new BallCenterMechanism();
-    	intakeShooter = new IntakeShooter();
-    	hook = new ScalingHook();
-    
-        chooser = new SendableChooser();
-        chooser.addDefault("Default Auto", defaultAuto);
-        chooser.addObject("My Auto", customAuto);
-        SmartDashboard.putData("Auto choices", chooser);
-        
-        // all smart dashboard information should go into the periodics
-        SmartDashboard.putNumber("Arm Pot", arm.getAngle());
-        SmartDashboard.putNumber("Shoulder Arm Speed", arm.getTestShoulderSpeed());
-
-        
-
-    }
-    
 	/**
-	 * This autonomous (along with the chooser code above) shows how to select between different autonomous modes
-	 * using the dashboard. The sendable chooser code works with the Java SmartDashboard. If you prefer the LabVIEW
-	 * Dashboard, remove all of the chooser code and uncomment the getString line to get the auto name from the text box
-	 * below the Gyro
-	 *
-	 * You can add additional auto modes by adding additional comparisons to the switch structure below with additional strings.
-	 * If using the SendableChooser make sure to add them to the chooser code above as well.
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
 	 */
-    public void autonomousInit() {
-    	autoSelected = (String) chooser.getSelected();
-//		autoSelected = SmartDashboard.getString("Auto Selector", defaultAuto);
+	public void robotInit() {
+		oi = new OI();
+		drive = new DriveBase();
+
+		arm = new ShoulderArm();
+		center = new BallCenterMechanism();
+		intakeShooter = new IntakeShooter();
+		hook = new ScalingHook();
+
+		chooser = new SendableChooser();
+		chooser.addDefault("Default Auto", defaultAuto);
+		chooser.addObject("My Auto", customAuto);
+		SmartDashboard.putData("Auto choices", chooser);
+
+		// all smart dashboard information should go into the periodics
+		SmartDashboard.putNumber("Arm Pot", arm.getAngle());
+		SmartDashboard.putNumber("Shoulder Arm Speed", arm.getTestShoulderSpeed());
+
+	}
+
+	/**
+	 * This autonomous (along with the chooser code above) shows how to select
+	 * between different autonomous modes using the dashboard. The sendable
+	 * chooser code works with the Java SmartDashboard. If you prefer the
+	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+	 * getString line to get the auto name from the text box below the Gyro
+	 *
+	 * You can add additional auto modes by adding additional comparisons to the
+	 * switch structure below with additional strings. If using the
+	 * SendableChooser make sure to add them to the chooser code above as well.
+	 */
+	public void autonomousInit() {
+		autoSelected = (String) chooser.getSelected();
+		// autoSelected = SmartDashboard.getString("Auto Selector",
+		// defaultAuto);
 		System.out.println("Auto selected: " + autoSelected);
-    }
+	}
 
-    /**
-     * This function is called periodically during autonomous
-     */
-    public void autonomousPeriodic() {
-    	
-    	SmartDashboard.putNumber("Arm Pot", arm.getAngle());
-    	SmartDashboard.putNumber("Shoulder Arm Speed", arm.getTestShoulderSpeed());
-        
-    	switch(autoSelected) {
-    	case customAuto:
-        //Put custom auto code here   
-            break;
-    	case defaultAuto:
-    	default:
-    	//Put default auto code here
-            break;
-    	}
-    }
+	/**
+	 * This function is called periodically during autonomous
+	 */
+	public void autonomousPeriodic() {
 
-    /**
-     * This function is called periodically during operator control
-     */
-    public void teleopPeriodic() {
-    	
-    	SmartDashboard.putNumber("Arm Pot", arm.getAngle());
-    	SmartDashboard.putNumber("Shoulder Arm Speed", arm.getTestShoulderSpeed());
+		SmartDashboard.putNumber("Arm Pot", arm.getAngle());
+		SmartDashboard.putNumber("Shoulder Arm Speed", arm.getTestShoulderSpeed());
 
-     
-        
-    	drive.setSpeed(oi.driver.getLeftY(),oi.driver.getRightY() );
-    	if (oi.operator.buttonPressed(Controller.LEFT_TRIGGER)) {
-    		hook.lowerHook();
-    	} else if (oi.operator.buttonPressed(Controller.LEFT_BUMPER)) {
-    		hook.raiseHook();
-    	} else {
-    		hook.setSpeed(oi.operator.getLeftY());
-    	}
-    	
-    	if (oi.operator.buttonPressed(Controller.DOWN_A_ABXY)) {
-    		intakeShooter.ballIntake();
-    	} else if (oi.operator.buttonPressed(Controller.RIGHT_B_ABXY)) {
-    		intakeShooter.ballShoot1();
-    	} else {
-    		intakeShooter.intakeShooterManual(oi.operator.getLeftX());
-    	}
-    	
-    	if (oi.operator.buttonPressed(Controller.LEFT_X_ABXY)) {
-    		center.ballCenter();
-    	} else if (oi.operator.buttonPressed(Controller.UP_Y_ABXY)) {
-    		center.ballreversal();
-    	}
-    	if (oi.operator.buttonPressed(Controller.RIGHT_BUMPER)){
-    		arm.shoulderManual(arm.UP_SPEED);
-    	} else if (oi.operator.buttonPressed(Controller.RIGHT_TRIGGER)) {
-    		arm.shoulderManual(arm.DOWN_SPEED);
-    	} else {
-    		arm.presetAngle(oi.operator.getDPad());
-    	}
-    	if (oi.operator.buttonPressed(Controller.START_BUTTON)){
-    		center.readyShot();
-    	}
-    	
-    	if (oi.operator.buttonPressed(Controller.LEFT_X_ABXY)){
-    		intakeShooter.ballShoot1();
-    	}
-    	else if (oi.operator.buttonPressed(Controller.UP_Y_ABXY)){
-    		intakeShooter.ballShoot2();
-    	}
-    	else if (oi.operator.buttonPressed(Controller.RIGHT_B_ABXY)){
-    		intakeShooter.ballIntake();
-    	}
-    
-    }
-    
-    /**
-     * This function is called periodically during test mode
-     */
-    public void testPeriodic() {
-    
-    }
-    
+		switch (autoSelected) {
+		case customAuto:
+			// Put custom auto code here
+			break;
+		case defaultAuto:
+		default:
+			// Put default auto code here
+			break;
+		}
+	}
+
+	/**
+	 * This function is called periodically during operator control
+	 */
+	public void teleopPeriodic() {
+
+		SmartDashboard.putNumber("Arm Pot", arm.getAngle());
+		SmartDashboard.putNumber("Shoulder Arm Speed", arm.getTestShoulderSpeed());
+
+		drive.setSpeed(oi.driver.getLeftY(), oi.driver.getRightY());
+		if (oi.operator.buttonPressed(Controller.LEFT_TRIGGER)) {
+			hook.lowerHook();
+		} else if (oi.operator.buttonPressed(Controller.LEFT_BUMPER)) {
+			hook.raiseHook();
+		} else {
+			hook.setSpeed(oi.operator.getLeftY());
+		}
+
+//		if (oi.operator.buttonPressed(Controller.DOWN_A_ABXY)) {
+//			intakeShooter.ballIntake();
+//		} else if (oi.operator.buttonPressed(Controller.RIGHT_B_ABXY)) {
+//			intakeShooter.ballShoot1();
+//		} else {
+//			intakeShooter.intakeShooterManual(oi.operator.getLeftX());
+//		}
+
+		if (oi.operator.buttonPressed(Controller.DOWN_A_ABXY)) {
+			center.ballCenter();
+//		} else if (oi.operator.buttonPressed(Controller.UP_Y_ABXY)) {
+//			center.ballreversal();
+		}
+		
+		if (oi.operator.buttonPressed(Controller.SELECT_BUTTON)) {
+			arm.stopShoulderArm();
+		} else if (oi.operator.buttonPressed(Controller.RIGHT_BUMPER)) {
+			arm.shoulderManualAngle(true);
+		} else if (oi.operator.buttonPressed(Controller.RIGHT_TRIGGER)) {
+			arm.shoulderManualAngle(false);
+		} else {
+			arm.presetAngle(oi.operator.getDPad());
+		}
+		arm.moveToAngle();
+
+		if (oi.operator.buttonPressed(Controller.START_BUTTON)) {
+			center.readyShot();
+		}
+
+		if (oi.operator.buttonPressed(Controller.LEFT_X_ABXY)) {
+			intakeShooter.ballShoot1();
+		} else if (oi.operator.buttonPressed(Controller.UP_Y_ABXY)) {
+			intakeShooter.ballShoot2();
+		} else if (oi.operator.buttonPressed(Controller.RIGHT_B_ABXY)) {
+			intakeShooter.ballIntake();
+		}
+
+	}
+
+	/**
+	 * This function is called periodically during test mode
+	 */
+	public void testPeriodic() {
+
+	}
+
 }
--- 2016Project/src/org/usfirst/frc/team131/robot/ShoulderArm.java
+++ 2016Project/src/org/usfirst/frc/team131/robot/ShoulderArm.java
@@ -3,55 +3,90 @@ package org.usfirst.frc.team131.robot;
 import edu.wpi.first.wpilibj.Talon;
 
 public class ShoulderArm {
+	private double goalAngle = -1;
+	private double angleChange = 5;
 	ChaosPot pot = new ChaosPot();
 	AngleSpeedModifier modifier = new AngleSpeedModifier();
 	private final double TOP = 90;
 	private final double BOTTOM = 0;
 	private final double UPPER_MIDDLE = 60;
 	private final double LOWER_MIDDLE = 30;
-	
+
 	private Talon leftShoulderTalon = new Talon(PortConstants.LEFT_SHOULDER_TALON);
 	private Talon rightShoulderTalon = new Talon(PortConstants.RIGHT_SHOULDER_TALON);
 	public final double DOWN_SPEED = 1.0;
 	public final double UP_SPEED = -1.0;
-	
-	public double getAngle () {
+
+	public double getAngle() {
 		return pot.getAngle();
 	}
-	public void shoulderManual (double speed) {
-		rightShoulderTalon.set(speed);
-		leftShoulderTalon.set(speed);
+
+	public void shoulderManualAngle(boolean isUp) {
+		if (isUp) {
+			goalAngle = goalAngle + angleChange;
+		} else {
+			goalAngle = goalAngle - angleChange;
+		}
+
 	}
-	public double getTestShoulderSpeed () {
+
+	public double getTestShoulderSpeed() {
 		return modifier.adjustSpeed(pot.getAngle(), UPPER_MIDDLE, UP_SPEED);
 	}
-	public void presetAngle (Controller.DPadDirection direction) {
-		double speed = 0;
-		switch(direction) {
+
+	public void presetAngle(Controller.DPadDirection direction) {
+		// double speed = 0;
+		switch (direction) {
 		case RIGHT:
-			speed = modifier.adjustSpeed(pot.getAngle(), UPPER_MIDDLE, UP_SPEED);
-			shoulderManual (speed);
-			System.out.println("right");
+			// speed = modifier.adjustSpeed(pot.getAngle(), UPPER_MIDDLE,
+			// UP_SPEED);
+			// shoulderManual (speed);
+			// System.out.println("right");
+			goalAngle = UPPER_MIDDLE;
 			break;
 		case LEFT:
-			speed = modifier.adjustSpeed(pot.getAngle(), LOWER_MIDDLE, DOWN_SPEED);
-			shoulderManual (speed);
-			System.out.println("left");
+			// speed = modifier.adjustSpeed(pot.getAngle(), LOWER_MIDDLE,
+			// DOWN_SPEED);
+			// shoulderManual (speed);
+			// System.out.println("left");
+			goalAngle = LOWER_MIDDLE;
 			break;
 		case UP:
-			speed = modifier.adjustSpeed(pot.getAngle(), TOP, UP_SPEED);
-			shoulderManual (speed);
-			System.out.println("up");
+			// speed = modifier.adjustSpeed(pot.getAngle(), TOP, UP_SPEED);
+			// shoulderManual (speed);
+			// System.out.println("up");
+			goalAngle = TOP;
 			break;
 		case DOWN:
-			speed = modifier.adjustSpeed(pot.getAngle(), BOTTOM, DOWN_SPEED);
-			shoulderManual (speed);
-			System.out.println("down");
+			// speed = modifier.adjustSpeed(pot.getAngle(), BOTTOM, DOWN_SPEED);
+			// shoulderManual (speed);
+			// System.out.println("down");
+			goalAngle = BOTTOM;
 			break;
 		default:
-			shoulderManual (speed);
-			System.out.println("none");
+			// shoulderManual (speed);
+			// System.out.println("none");
 			break;
 		}
+
+	}
+
+	public void stopShoulderArm() {
+		goalAngle = -1;
+	}
+
+	public void moveToAngle() {
+		if (goalAngle != -1) {
+			double speed = modifier.adjustSpeed(pot.getAngle(), goalAngle, UP_SPEED);
+			setShoulderSpeed(speed);
+		}
+
+		// TODO Auto-generated method stub
+
+	}
+
+	private void setShoulderSpeed(double speed) {
+		leftShoulderTalon.set(speed);
+		rightShoulderTalon.set(speed);
 	}
 }
