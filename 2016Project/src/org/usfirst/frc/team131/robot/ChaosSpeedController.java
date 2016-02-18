d2b94dc924b2a6c0d909727d5d63ee605317e7ce
ashnh <ashnhx@gmail.com>
2016-02-18 18:37:37 -0500

    changing drive base to use robot drive

--- 2016Project/src/org/usfirst/frc/team131/robot/ChaosSpeedController.java
+++ 2016Project/src/org/usfirst/frc/team131/robot/ChaosSpeedController.java
@@ -2,75 +2,77 @@ package org.usfirst.frc.team131.robot;
 
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.Victor;
-
+	
 public class ChaosSpeedController implements SpeedController {
 
-	private Victor firstVictor;
-	private Victor secondVictor;
-	private Victor thirdVictor;
-	private boolean isInverted = false;
-	private boolean isDisabled = false;
-	private double power = 0;
+		private Victor firstVictor;
+		private Victor secondVictor;
+		private Victor thirdVictor;
+		private boolean isInverted = false;
+		private boolean isDisabled = false;
+		private double power = 0;
 
-	public ChaosSpeedController(int port1, int port2, int port3) {
-		firstVictor = new Victor(port1);
-		secondVictor = new Victor(port2);
-		thirdVictor = new Victor(port3);
+		public ChaosSpeedController(int port1, int port2, int port3) {
+			firstVictor = new Victor(port1);
+			secondVictor = new Victor(port2);
+			thirdVictor = new Victor(port3);
+
+		}
+
+		@Override
+		public void pidWrite(double output) {
+			firstVictor.pidWrite(output);
+			secondVictor.pidWrite(output);
+			thirdVictor.pidWrite(output);
+
+		}
+
+		@Override
+		public double get() {
+
+			return power;
+		}
+
+		@Override
+		public void set(double speed, byte syncGroup) {
+			this.set(speed);
+
+		}
+
+		@Override
+		public void set(double speed) {
+			power = speed;
+
+			firstVictor.set(speed);
+			secondVictor.set(speed);
+			thirdVictor.set(speed);
+
+		}
+
+		@Override
+		public void setInverted(boolean isInverted) {
+			this.isInverted = isInverted;
+			firstVictor.setInverted(isInverted);
+			secondVictor.setInverted(isInverted);
+			thirdVictor.setInverted(isInverted);
+		}
+
+		@Override
+		public boolean getInverted() {
+			return isInverted;
+		}
+
+		@Override
+		public void disable() {
+			this.set(0);
+			isDisabled = true;
+
+			firstVictor.disable();
+			secondVictor.disable();
+			thirdVictor.disable();
+
+		}
 
 	}
 
-	@Override
-	public void pidWrite(double output) {
-		firstVictor.pidWrite(output);
-		secondVictor.pidWrite(output);
-		thirdVictor.pidWrite(output);
 
-	}
-
-	@Override
-	public double get() {
-
-		return power;
-	}
-
-	@Override
-	public void set(double speed, byte syncGroup) {
-		this.set(speed);
-
-	}
-
-	@Override
-	public void set(double speed) {
-		power = speed;
-
-		firstVictor.set(speed);
-		secondVictor.set(speed);
-		thirdVictor.set(speed);
-
-	}
-
-	@Override
-	public void setInverted(boolean isInverted) {
-		this.isInverted = isInverted;
-		firstVictor.setInverted(isInverted);
-		secondVictor.setInverted(isInverted);
-		thirdVictor.setInverted(isInverted);
-	}
-
-	@Override
-	public boolean getInverted() {
-		return isInverted;
-	}
-
-	@Override
-	public void disable() {
-		this.set(0);
-		isDisabled = true;
-		
-		firstVictor.disable();
-		secondVictor.disable();
-		thirdVictor.disable();
-
-	}
-
-}
--- 2016Project/src/org/usfirst/frc/team131/robot/DriveBase.java
+++ 2016Project/src/org/usfirst/frc/team131/robot/DriveBase.java
@@ -1,101 +1,126 @@
 package org.usfirst.frc.team131.robot;
 
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Victor;
 
 /**
  * This class is the drive base
+ * 
  * @author Charles
  *
  */
-public class DriveBase {
+public class DriveBase extends RobotDrive{
 	private static final double ENCODER_PULSES_PER_REVOLUTION = 360.0;
 	private static final double ENCODER_GEAR_RATIO = 1.0;
 	private final double wheelCircumference = 8.0 * Math.PI;
-	
-	Victor frontLeft = new Victor(PortConstants.LEFT_FRONT_VICTOR_PORT);
-	Victor middleLeft = new Victor (PortConstants.LEFT_MIDDLE_VICTOR_PORT);
-	Victor backLeft = new Victor (PortConstants.LEFT_BACK_VICTOR_PORT);
-	
-	Victor frontRight = new Victor (PortConstants.RIGHT_FRONT_VICTOR_PORT);
-	Victor middleRight = new Victor (PortConstants.RIGHT_MIDDLE_VICTOR_PORT);
-	Victor backRight = new Victor (PortConstants.RIGHT_BACK_VICTOR_PORT);
-	
-	Encoder rightEncoder = new Encoder(PortConstants.RIGHT_ENCODER_PORT_A, PortConstants.RIGHT_ENCODER_PORT_A, false, Encoder.EncodingType.k4X);
-	Encoder leftEncoder = new Encoder(PortConstants.LEFT_ENCODER_PORT_A, PortConstants.LEFT_ENCODER_PORT_B, false, Encoder.EncodingType.k4X);	
-	
+
+	/*
+	 * Victor frontLeft = new Victor(PortConstants.LEFT_FRONT_VICTOR_PORT);
+	 * Victor middleLeft = new Victor (PortConstants.LEFT_MIDDLE_VICTOR_PORT);
+	 * Victor backLeft = new Victor (PortConstants.LEFT_BACK_VICTOR_PORT);
+	 * 
+	 * Victor frontRight = new Victor (PortConstants.RIGHT_FRONT_VICTOR_PORT);
+	 * Victor middleRight = new Victor (PortConstants.RIGHT_MIDDLE_VICTOR_PORT);
+	 * Victor backRight = new Victor (PortConstants.RIGHT_BACK_VICTOR_PORT);
+	 */
+
+	private static ChaosSpeedController rightSide = new ChaosSpeedController(
+			PortConstants.RIGHT_FRONT_VICTOR_PORT,
+			PortConstants.RIGHT_MIDDLE_VICTOR_PORT,
+			PortConstants.RIGHT_BACK_VICTOR_PORT);
+
+	private static ChaosSpeedController leftSide = new ChaosSpeedController(
+			PortConstants.LEFT_FRONT_VICTOR_PORT,
+			PortConstants.LEFT_MIDDLE_VICTOR_PORT,
+			PortConstants.LEFT_BACK_VICTOR_PORT);
+
+			
+	Encoder rightEncoder = new Encoder(PortConstants.RIGHT_ENCODER_PORT_A, PortConstants.RIGHT_ENCODER_PORT_A, false,
+			Encoder.EncodingType.k4X);
+	Encoder leftEncoder = new Encoder(PortConstants.LEFT_ENCODER_PORT_A, PortConstants.LEFT_ENCODER_PORT_B, false,
+			Encoder.EncodingType.k4X);
+
 	double leftDirection = -1.0;
 	double rightDirection = 1.0;
-	
-	public void reverseDirection () {
+
+	public void reverseDirection() {
 		rightDirection *= -1.0;
 		leftDirection *= -1.0;
 	}
-	
+
 	/**
 	 * This function gets the distance traveled of the right side motors
+	 * 
 	 * @return (distance traveled)
 	 */
-	public double getRightDistance () {
+	public double getRightDistance() {
 		return rightEncoder.getDistance();
 	}
-	
+
 	/**
 	 * This function gets the distance traveled of the left side motors
+	 * 
 	 * @return (distance traveled)
 	 */
-	public double getLeftDistance () {
+	public double getLeftDistance() {
 		return leftEncoder.getDistance();
 	}
-	
+
 	/**
 	 * This function sets the speed of the drive
-	 * @param leftSpeed (speed of left motors)
-	 * @param rightSpeed (speed of right motors
+	 * 
+	 * @param leftSpeed
+	 *            (speed of left motors)
+	 * @param rightSpeed
+	 *            (speed of right motors
 	 */
-	void setSpeed (double leftSpeed, double rightSpeed) {
-		frontLeft.set(leftDirection * leftSpeed);
+	void setSpeed(double leftSpeed, double rightSpeed) {
+		/*frontLeft.set(leftDirection * leftSpeed);
 		middleLeft.set(leftDirection * leftSpeed);
 		backLeft.set(leftDirection * leftSpeed);
-		
+
 		frontRight.set(rightDirection * rightSpeed);
 		middleRight.set(rightDirection * rightSpeed);
-		backRight.set(rightDirection * rightSpeed);
+		backRight.set(rightDirection * rightSpeed);*/
+		
+		tankDrive(leftDirection*leftSpeed, rightDirection*rightSpeed);
 	}
 	
-	public double getLeftRotationalDistance () {
+	public double getLeftRotationalDistance() {
 		return rightEncoder.getDistance();
 	}
-	
-	public double getRightRotationalDistance () {
+
+	public double getRightRotationalDistance() {
 		return leftEncoder.getDistance();
 	}
-	
-	public double getleftDistanceInInches () {
+
+	public double getleftDistanceInInches() {
 		return wheelCircumference * getLeftRotationalDistance();
 	}
-	
-	public double getRightDistanceInInches () {
+
+	public double getRightDistanceInInches() {
 		return wheelCircumference * getRightRotationalDistance();
 	}
-	
+
 	/**
 	 * This is the constructor for the encoders
 	 */
 	public DriveBase() {
-		// TODO Auto-generated constructor stub
-		double distancePerPulse = ENCODER_GEAR_RATIO / ENCODER_PULSES_PER_REVOLUTION;
+		super(rightSide, leftSide);
 		
+		double distancePerPulse = ENCODER_GEAR_RATIO / ENCODER_PULSES_PER_REVOLUTION;
+
 		leftEncoder.setMaxPeriod(0.1);
 		leftEncoder.setDistancePerPulse(distancePerPulse);
 		leftEncoder.setReverseDirection(true);
 		leftEncoder.setSamplesToAverage(7);
-		
+
 		rightEncoder.setMaxPeriod(0.1);
 		rightEncoder.setDistancePerPulse(distancePerPulse);
 		rightEncoder.setReverseDirection(true);
 		rightEncoder.setSamplesToAverage(7);
-		
+
 	}
 
 }
