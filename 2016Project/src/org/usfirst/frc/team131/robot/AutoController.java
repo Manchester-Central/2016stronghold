ce8e7145dfa93d98742a2aed769324de51f5cdf0
Katie <kt_roll@yahoo.com>
2016-02-18 19:50:35 -0500

    Added autoController. Reformatted autonomous

---
+++ 2016Project/src/org/usfirst/frc/team131/robot/AutoController.java
@@ -0,0 +1,128 @@
+package org.usfirst.frc.team131.robot;
+
+import org.usfirst.frc.team131.robot.Controller.DPadDirection;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+
+public class AutoController {
+
+	private static final double INCHES_TO_CROSS_DEFENSE = 96;
+
+	public static enum autoState {
+		START, DRIVE_FORWARD, DRIVE_BACKWARD, TURN_CLOCKWISE, STOP, SET_ARM_FORWARD, SET_ARM_SHOOT, SET_ARM_BACKWARD, SET_ARM_BACKWARD_RAMP, SET_FLYWHEEL_SPEED, SHOOT
+
+	}
+
+	private autoState autoCase;
+
+	public void autoStateBackward(ShoulderArm arm, DriveBase drive) {
+		switch (autoCase) {
+
+		case SET_ARM_BACKWARD_RAMP:
+			arm.presetAngle(DPadDirection.UP);
+			break;
+		case DRIVE_BACKWARD:
+			drive.setSpeed(-0.2, -0.2);
+			break;
+		case STOP:
+			drive.setSpeed(0, 0);
+			break;
+		default:
+
+			break;
+		}
+		if (autoCase == autoState.START) {
+			autoCase = autoState.SET_ARM_BACKWARD_RAMP;
+		}
+		if (arm.getAngle() == arm.getAngleSetpoint()) {
+			autoCase = autoState.DRIVE_BACKWARD;
+		}
+		if (drive.getRightDistanceInInches() >= INCHES_TO_CROSS_DEFENSE
+				&& drive.getleftDistanceInInches() >= INCHES_TO_CROSS_DEFENSE) {
+			autoCase = autoState.STOP;
+		}
+	}
+
+	public void autoStateForward(ShoulderArm arm, DriveBase drive) {
+		switch (autoCase) {
+
+		case SET_ARM_FORWARD:
+			arm.presetAngle(DPadDirection.DOWN);
+			break;
+		case DRIVE_FORWARD:
+			drive.setSpeed(0.2, 0.2);
+			break;
+		case STOP:
+			drive.setSpeed(0, 0);
+			break;
+		default:
+
+			break;
+		}
+		if (autoCase == autoState.START) {
+			autoCase = autoState.DRIVE_FORWARD;
+		}
+		if (arm.getAngle() == arm.getAngleSetpoint()) {
+			autoCase = autoState.DRIVE_BACKWARD;
+		}
+		if (drive.getRightDistanceInInches() >= INCHES_TO_CROSS_DEFENSE
+				&& drive.getleftDistanceInInches() >= INCHES_TO_CROSS_DEFENSE) {
+			autoCase = autoState.STOP;
+		}
+	}
+
+	public void autoStateSpy(ShoulderArm arm, DriveBase drive, IntakeShooter intakeShooter, BallCenterMechanism center,
+			DigitalInput frontOpticalSensorA, DigitalInput frontOpticalSensorB) {
+		switch (autoCase) {
+		case SET_ARM_SHOOT:
+			arm.presetAngle(DPadDirection.LEFT);
+			break;
+		case DRIVE_BACKWARD:
+			drive.setSpeed(-0.2, -0.2);
+			break;
+		case TURN_CLOCKWISE:
+			// may need to alter
+			drive.setSpeed(0.1, -0.1);
+			break;
+		case DRIVE_FORWARD:
+			drive.setSpeed(0.2, 0.2);
+			break;
+		case SET_FLYWHEEL_SPEED:
+			intakeShooter.ballShoot1();
+			break;
+		case SHOOT:
+			center.readyShot();
+			break;
+		default:
+			break;
+		}
+		intakeShooter.updateFlywheelSpeed();
+		if (autoCase == autoState.START) {
+			autoCase = autoState.SET_ARM_SHOOT;
+		}
+		if (arm.getAngle() == arm.getAngleSetpoint()) {
+			autoCase = autoState.DRIVE_BACKWARD;
+		}
+		if (drive.getleftDistanceInInches() >= 96 && drive.getRightDistanceInInches() >= 96) {
+			autoCase = autoState.TURN_CLOCKWISE;
+		}
+		if (frontOpticalSensorA.get() && frontOpticalSensorB.get()) {
+			autoCase = autoState.DRIVE_FORWARD;
+		}
+		if (drive.getleftDistanceInInches() >= 196 && drive.getRightDistanceInInches() >= 196) {
+			autoCase = autoState.SET_FLYWHEEL_SPEED;
+		}
+		if (intakeShooter.checkShooterSpeed(1)) {
+			autoCase = autoState.SHOOT;
+		}
+	}
+
+	public void autoStateDefault(DriveBase drive) {
+		if (drive.getRightDistanceInInches() <= INCHES_TO_CROSS_DEFENSE
+				&& drive.getleftDistanceInInches() <= INCHES_TO_CROSS_DEFENSE) {
+			drive.setSpeed(0.2, 0.2);
+		} else {
+			drive.setSpeed(0, 0);
+		}
+	}
+}
