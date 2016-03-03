f76f132f221ed84ee9f20081809abd431ac544d9
unknown <easonsu1129@gmail.com>
2016-03-03 18:03:27 -0500

    changed flywheelUpToSpeed check from 2000 to 5500, changed the dashboard design, and added forward auto state to dashboard

--- 2016Project/src/org/usfirst/frc/team131/robot/Test.java
+++
@@ -1,9 +0,0 @@
-package org.usfirst.frc.team131.robot;
-
-import edu.wpi.first.wpilibj.command.CommandGroup;
-
-public class Test extends CommandGroup{
-	public void test (DriveBase drive) {
-		drive.setSpeed(0.2, 0.2);
-	}
-}
